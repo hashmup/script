2c04cd0253d561069b77f6a69320845c09bd44b6
Yuta Inoue <y.wizowoz@gmail.com>
2015-02-27 21:03:04 +0900

    edit setup.sh

---
+++ osx/setup_yosemite.sh
@@ -0,0 +1,14 @@
+#if /usr/bin/xcodebuild >/dev/null; then
+  . brew.sh
+  . macinit.sh
+  . ~/script/common/vim.sh
+  . ~/script/common/unlink.sh
+  . ~/script/common/link.sh
+  mkdir ~/vagrantBoxes
+  . ~/javascript/npm.sh
+  . ~/javascript/bower.sh
+#else
+#  echo git command not exist
+#  java -version
+#  xcode-select --install
+#fi
